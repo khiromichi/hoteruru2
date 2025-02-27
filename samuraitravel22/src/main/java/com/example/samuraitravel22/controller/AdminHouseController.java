4a77bb56963b22c9cc01fd64d712774a3e12765f
hiro <hiro@192.168.10.110>
2025-02-27 14:16:18 +0900

    202502271400

--- samuraitravel22/src/main/java/com/example/samuraitravel22/controller/AdminHouseController.java
+++ samuraitravel22/src/main/java/com/example/samuraitravel22/controller/AdminHouseController.java
@@ -1,13 +1,14 @@
 package com.example.samuraitravel22.controller;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.samuraitravel22.entity.House;
 import com.example.samuraitravel22.repository.HouseRepository;
@@ -16,19 +17,28 @@ import com.example.samuraitravel22.repository.HouseRepository;
 @RequestMapping("/admin/houses")
 public class AdminHouseController {
 	private final HouseRepository houseRepository;
-	
+
 	public AdminHouseController(HouseRepository houseRepository) {
 		this.houseRepository = houseRepository;
 	}
-	
+
 	@GetMapping
-	public String index(Model model, Pageable pageable) {
-		List<House> houses = houseRepository.findAll();
-		Page<House> housePage = houseRepository.findAll(pageable);
-		
-		model.addAttribute("houses", houses);
+	public String index(Model model,
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+			@RequestParam(name = "keyword", required = false) String keyword) {
+		//		Page<House> housePage = houseRepository.findAll(pageable);
+		Page<House> housePage;
+
+		if (keyword != null && !keyword.isEmpty()) {
+
+			housePage = houseRepository.findByNameLike("%" + keyword + "%", pageable);
+		} else {
+			housePage = houseRepository.findAll(pageable);
+		}
+
 		model.addAttribute("housePage", housePage);
-		
+		model.addAttribute("keyword", keyword);
+
 		return "admin/houses/index";
 	}
 }
