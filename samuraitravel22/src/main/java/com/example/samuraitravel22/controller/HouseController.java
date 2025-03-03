eace1d739aabbbae399226fa266ba3dca616b44f
hiro <hiro@192.168.10.110>
2025-03-03 15:00:48 +0900

    202503031500

--- samuraitravel22/src/main/java/com/example/samuraitravel22/controller/HouseController.java
+++ samuraitravel22/src/main/java/com/example/samuraitravel22/controller/HouseController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.samuraitravel22.entity.House;
+import com.example.samuraitravel22.form.ReservationInputForm;
 import com.example.samuraitravel22.repository.HouseRepository;
 
 @Controller
@@ -76,7 +77,8 @@ public class HouseController {
     public String show(@PathVariable(name = "id") Integer id, Model model) {
         House house = houseRepository.getReferenceById(id);
         
-        model.addAttribute("house", house);         
+        model.addAttribute("house", house);
+        model.addAttribute("reservationInputForm", new ReservationInputForm());
         
         return "houses/show";
     }    
