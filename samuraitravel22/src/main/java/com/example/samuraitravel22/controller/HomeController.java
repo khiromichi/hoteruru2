132ed02612f67cfbf9f8da68553fc1028ca519cb
hiro <hiro@192.168.10.110>
2025-03-03 10:03:12 +0900

    202503031000

--- samuraitravel22/src/main/java/com/example/samuraitravel22/controller/HomeController.java
+++ samuraitravel22/src/main/java/com/example/samuraitravel22/controller/HomeController.java
@@ -1,12 +1,28 @@
 package com.example.samuraitravel22.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.samuraitravel22.entity.House;
+import com.example.samuraitravel22.repository.HouseRepository;
 
 @Controller
 public class HomeController {
-     @GetMapping("/")
-    public String index() {
-         return "index";
-     }   
+    private final HouseRepository houseRepository;        
+    
+    public HomeController(HouseRepository houseRepository) {
+        this.houseRepository = houseRepository;            
+    }    
+   
+   @GetMapping("/")
+    public String index(Model model) {
+        List<House> newHouses = houseRepository.findTop10ByOrderByCreatedAtDesc();
+        model.addAttribute("newHouses", newHouses);        
+       
+       return "index";
+   }   
 }
+
