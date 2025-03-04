8ee83890754628cae0c34103b40f3e69c69da306
hiro <hiro@192.168.10.110>
2025-03-04 18:42:35 +0900

    202503041900

--- samuraitravel22/src/main/java/com/example/samuraitravel22/controller/ReservationController.java
+++ samuraitravel22/src/main/java/com/example/samuraitravel22/controller/ReservationController.java
@@ -15,7 +15,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.samuraitravel22.entity.House;
@@ -27,17 +26,22 @@ import com.example.samuraitravel22.repository.HouseRepository;
 import com.example.samuraitravel22.repository.ReservationRepository;
 import com.example.samuraitravel22.security.UserDetailsImpl;
 import com.example.samuraitravel22.service.ReservationService;
+import com.example.samuraitravel22.service.StripeService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class ReservationController {
     private final ReservationRepository reservationRepository;   
      private final HouseRepository houseRepository;
      private final ReservationService reservationService; 
+     private final StripeService stripeService;
       
-     public ReservationController(ReservationRepository reservationRepository, HouseRepository houseRepository, ReservationService reservationService) {   
+     public ReservationController(ReservationRepository reservationRepository, HouseRepository houseRepository, ReservationService reservationService, StripeService stripeService) {   
         this.reservationRepository = reservationRepository; 
          this.houseRepository = houseRepository;
          this.reservationService = reservationService;
+         this.stripeService = stripeService;
     }    
 
     @GetMapping("/reservations")
@@ -82,7 +86,7 @@ public class ReservationController {
      @GetMapping("/houses/{id}/reservations/confirm")
      public String confirm(@PathVariable(name = "id") Integer id,
                            @ModelAttribute ReservationInputForm reservationInputForm,
-                           @AuthenticationPrincipal UserDetailsImpl userDetailsImpl,                          
+                           @AuthenticationPrincipal UserDetailsImpl userDetailsImpl,HttpServletRequest httpServletRequest,                         
                            Model model) 
      {        
          House house = houseRepository.getReferenceById(id);
@@ -98,18 +102,21 @@ public class ReservationController {
          
          ReservationRegisterForm reservationRegisterForm = new ReservationRegisterForm(house.getId(), user.getId(), checkinDate.toString(), checkoutDate.toString(), reservationInputForm.getNumberOfPeople(), amount);
          
+         String sessionId = stripeService.createStripeSession(house.getName(), reservationRegisterForm, httpServletRequest);
+         
          model.addAttribute("house", house);  
-         model.addAttribute("reservationRegisterForm", reservationRegisterForm);       
+         model.addAttribute("reservationRegisterForm", reservationRegisterForm);     
+         model.addAttribute("sessionId", sessionId);
          
          return "reservations/confirm";
      }
      
-     @PostMapping("/houses/{id}/reservations/create")
-     public String create(@ModelAttribute ReservationRegisterForm reservationRegisterForm) {                
-         reservationService.create(reservationRegisterForm);        
-         
-         return "redirect:/reservations?reserved";
-     }
+//     @PostMapping("/houses/{id}/reservations/create")
+//     public String create(@ModelAttribute ReservationRegisterForm reservationRegisterForm) {                
+//         reservationService.create(reservationRegisterForm);        
+//         
+//         return "redirect:/reservations?reserved";
+//     }
 
 }
 
